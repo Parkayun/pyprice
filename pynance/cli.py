99a6469dd9282c6e230272e22a4616c739b71bd6
Parkayun <iamparkayun@gmail.com>
2015-07-16 15:51:29 +0900

    Added cli

---
+++ pynance/cli.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+import sys
+
+from .index import current, search
+
+
+def runner():
+	if len(sys.argv) == 1:
+		raise ValueError('Please type the command.')
+	elif len(sys.argv) == 2:
+		option, keyword = 'current', sys.argv[1]
+	elif len(sys.argv) == 3:
+		option, keyword = sys.argv[1], sys.argv[2]
+
+	if option == 'current':
+		try:
+			data = current(keyword)
+
+			print('Keyword:', data['keys']['displaySymbol'])
+			print('Value:', data['value'])
+			print('Change:', data['change']+',', data['pctChange']+'%')
+			print('Market:', data['exchange'])
+		except ValueError:
+			print('Can not find keyword. Try \'pynance search '+keyword+'\'')
+
+	elif option == 'search':
+		results = search(keyword)
+		if len(results) == 0:
+			print('Can not find keyword.')
+		else:
+			for result in results:
+				data = result[0]
+				print('Keyword:', data['s'])
+				print('Name:', data['n'], '\n')
+
+	else:
+		raise ValueError('Not supported option.')
+
+
+if __name__ == '__main__':
+	runner()
